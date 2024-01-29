5627314198a8d722a83042fe037b3c9c7b876689
Federico Poggi <federicopoggi3@gmail.com>
2024-01-29 11:19:03 +0100

    Initial commit

---
+++ src/main/java/team6/epicenergyspa/EpicEnergySpaApplication.java
@@ -0,0 +1,13 @@
+package team6.epicenergyspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergySpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicEnergySpaApplication.class, args);
+    }
+
+}
