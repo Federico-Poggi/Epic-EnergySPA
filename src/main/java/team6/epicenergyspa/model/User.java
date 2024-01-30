dc5a4294c21a3c8e40dadf54524515e8438dcfe9
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-01-30 04:41:25 +0100

    add auth side , some exceptions, Role enum, User , user service and add relative dto and dao

--- src/main/java/team6/epicenergyspa/model/User.java
+++ src/main/java/team6/epicenergyspa/model/User.java
@@ -1,4 +1,64 @@
 package team6.epicenergyspa.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private String username;
+    private Role role;
+
+    public User(String email, String password, String name, String surname, String username, Role role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    // avatar
+
 }
---
+++ src/main/java/team6/epicenergyspa/repositories/UserDAO.java
@@ -0,0 +1,13 @@
+package team6.epicenergyspa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team6.epicenergyspa.model.Address;
+import team6.epicenergyspa.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDAO extends JpaRepository<User,Long > {
+    Optional<User> findByEmail(String email);
+}
